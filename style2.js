c1e0a21830829d08e619d17ac33335d645eae088
Anjala-pv <82668694+Anjala-pv@users.noreply.github.com>
2021-05-15 14:54:38 +0530

    Add files via upload

---
+++ style2.js
@@ -0,0 +1,3 @@
+function s(v) { document.getElementById('res').value = v }
+function a(v) { document.getElementById('res').value += v }
+function e() { try { s(eval(document.getElementById('res').value)) } catch(e) { s('Error') } }
